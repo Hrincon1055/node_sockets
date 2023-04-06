54bca19cbff7c76f7bfe2862f5015e60b24b0df2
Henry Rincon <henryrinconsantana@gmail.com>
2023-04-06 14:11:40 -0500

    first commit

--- routes/router.ts
+++ routes/router.ts
@@ -1,4 +1,6 @@
 import { Router, Request, Response } from 'express';
+import Server from '../classes/server';
+
 const router = Router();
 
 router.get('/mensajes', (req: Request, res: Response) => {
@@ -8,17 +10,35 @@ router.get('/mensajes', (req: Request, res: Response) => {
   });
 });
 router.post('/mensajes', (req: Request, res: Response) => {
-  console.log('router LINE 12 =>', req.body);
+  const cuerpo = req.body.cuerpo;
+  const de = req.body.de;
+  const server = Server.instance;
+  const payload = {
+    de,
+    cuerpo,
+  };
+  server.io.emit('mensaje-privado', payload);
   res.json({
     ok: true,
-    mensaje: 'TODO OK POST 1',
+    cuerpo,
+    de,
   });
 });
 router.post('/mensajes/:id', (req: Request, res: Response) => {
-  console.log('router LINE 18 =>', req.body, req.params);
+  const cuerpo = req.body.cuerpo;
+  const de = req.body.de;
+  const id = req.params.id;
+  const server = Server.instance;
+  const payload = {
+    de,
+    cuerpo,
+  };
+  server.io.in(id).emit('mensaje-privado', payload);
   res.json({
     ok: true,
-    mensaje: 'TODO OK POST 2',
+    cuerpo,
+    de,
+    id,
   });
 });
 export default router;
